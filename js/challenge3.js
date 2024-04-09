b9b4505431822853bddec7f4b5eedac94ce7df18
Julia Tauro <jtauro@Julias-MacBook-Pro.local>
2024-04-09 17:48:37 -0400

    challenge 3

---
+++ js/challenge3.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+  // this.alert(document.querySelectorAll('figcaption')[1].innerHTML);
+
+  let figcaptions = document.querySelectorAll('figcaption');
+  let secondFigcaptio = figcaptions[1];
+  this.alert(secondFigcaptio.innerHTML);
+  // let text = secondFigcaptio.innerHTML;
+  // let text = secondFigcaptio.textContent;
+
+  // secondFigcaptio.text
+});
--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let colorPicker = document.querySelector('#primary-picker');
 
-colorPicker.addEventListener("change", function(event)) {
-  console.log("");
-  let color = 
+colorPicker.addEventListener("change", function(event) {
+  let color = event.target.value;
 
-  let body = 
-  body.style.
-}
+  let body = document.querySelector('body');
+  body.style.backgroundColor = color;
+});
+
+// https://docs.google.com/presentation/d/1bfA4EKiZCbcKedYG1ASQlZ9MES36XIRhAAXd6iB_2no/edit
