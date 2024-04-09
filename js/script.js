fbf1c8682412b012d4591b4ee0de0f0c1262bde1
Julia Tauro <jtauro@Julias-MacBook-Pro.local>
2024-04-09 17:28:44 -0400

    init commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let colorPicker = document.querySelector('#primary-picker');
+
+colorPicker.addEventListener("change", function(event)) {
+  console.log("");
+  let color = 
+
+  let body = 
+  body.style.
+}
